e3a96cf16b70cd368d4a4ad72b10139e6f70ad91
LeWimbes <leon.wimbes@web.de>
2022-01-10 11:14:40 +0100

    remove tests, docs, etc

--- fuzz/fuzz_targets/fuzz_wssenddata.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use dtn7_plus::client::WsSendData;
-use libfuzzer_sys::fuzz_target;
-
-fuzz_target!(|data: &[u8]| {
-    // fuzzed code goes here
-    serde_cbor::from_slice::<WsSendData>(&data);
-});
